61f54a96e0e16935854aa3d6c437c6fc2e0c13dd
EdinK1 <edinkaymakchi@gmail.com>
2019-08-09 01:27:00 -0700

    clean up home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,9 +11,6 @@ const IndexPage = () => {
       <Head title="Home" />
       <h1>Hello</h1>
       <h2>I'm Edin, a full-stack developer living in Vancouver</h2>
-      <p>
-        Need a Developer ? <Link to="/contact">Contact me.</Link>
-      </p>
     </Layout>
   )
 }
